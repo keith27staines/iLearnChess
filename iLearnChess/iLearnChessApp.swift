ec1e30b0e8f6989e0397ebe10df09ffd16867512
Keith Staines <keith.staines@workfinder.com>
2021-11-21 12:15:46 +0000

    Initial Commit

---
+++ iLearnChess/iLearnChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  iLearnChessApp.swift
+//  iLearnChess
+//
+//  Created by Keith Staines on 21/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct iLearnChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
